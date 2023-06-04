040712b4c6cf5220a6ef1bc47e7ae4799e98b4ee
HEYAHONG <2229388563@qq.com>
2023-06-04 18:10:36 +0800

    修改PATH顺序

--- config.bat
+++ config.bat
@@ -2,7 +2,7 @@
 set PROJECT_PATH=%~dp0
 cd /d %PROJECT_PATH%
 set PROJECT_PATH_FIX=%PROJECT_PATH:\=/%
-set PATH=%PROJECT_PATH%\tools\bin;%PATH%
+set PATH=%PATH%;%PROJECT_PATH%\tools\bin
 
 set RTTHREADDIR=%PROJECT_PATH_FIX%/rt-thread
 set RTTHREADBSPDIR=%PROJECT_PATH_FIX%/rt-thread/bsp/wch/arm/ch579m
